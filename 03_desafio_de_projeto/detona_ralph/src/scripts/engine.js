770a2065e1c1ac98ae019575abdae2a25bc83e2e
Douglas Rodrigues <163287204+Douglas-Luiz-de-Oliveira-Rodrigues@users.noreply.github.com>
2025-09-05 19:02:35 -0300

    03_desafio_de_projeto
    Desafio concluído em breve farei atualizações.

--- 03_desafio_de_projeto/detona_ralph/src/scripts/engine.js
+++ 03_desafio_de_projeto/detona_ralph/src/scripts/engine.js
@@ -6,9 +6,32 @@ const state = {
         score: document.querySelector("#score"),
     },
     values: {
-        timerId: null,
-        gameVelocity: 1000
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
     },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+};
+
+function playSound() {
+    let audio = new Audio("../audios/hit.mp3");
+    audio.volume = 0.2;
+    audio.play();
 };
 
 function randomSquare() {
@@ -19,18 +42,24 @@ function randomSquare() {
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.square[randomNumber];
     randomSquare.classList.add("enemy");
-}
-
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
-}
+    state.values.hitPosition = randomSquare.id;
+};
 
 function addListenerHitBox() {
-    state.view.square.forEach((square) => {});
+    state.view.square.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound();
+            }
+        });
+    });
 };
 
 function init() {
-    moveEnemy();
+    addListenerHitBox();
 };
 
 init();
